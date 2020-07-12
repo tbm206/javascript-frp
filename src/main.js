ef23a92291072ace94802ac03c179b36392bf5a8
Taha Ben Masaud <tbm206@gmail.com>
2020-07-12 20:01:43 +0100

    basic testing of cyclejs

--- src/main.js
+++ src/main.js
@@ -2,19 +2,78 @@ import xs from 'xstream';
 import {run} from '@cycle/xstream-run';
 import {makeDOMDriver} from '@cycle/dom';
 import Snabbdom from 'snabbdom-pragma';
+import {
+  add,
+  getHours,
+  getMinutes,
+  getSeconds,
+  format,
+} from 'date-fns';
+
+const TIME_FORMAT = 'H:m';
+const formatTime = t => format(t, TIME_FORMAT);
+
+const date = new Date();
+
+const timeNow$ = xs.periodic(1000)
+  .map(t => add(date, { seconds: t }));
+
+const midnight$ = timeNow$ 
+    .filter(t => [getHours, getMinutes, getSeconds].every(f => f(t) === 0))
+    .map(t => ({
+      fajir: formatTime(add(t, { hours: 4 })),
+      duhr: formatTime(add(t, { hours: 13 })),
+      asr: formatTime(add(t, { hours: 17 })),
+      maghrib: formatTime(add(t, { hours: 21 })),
+      isha: formatTime(add(t, { hours: 23 })),
+    }))
+    .startWith({
+      fajir: formatTime(add(date, { hours: 4 })),
+      duhr: formatTime(add(date, { hours: 13 })),
+      asr: formatTime(add(date, { hours: 17 })),
+      maghrib: formatTime(add(date, { hours: 21 })),
+      isha: formatTime(add(date, { hours: 23 })),
+    });
 
 const drivers = {
   DOM: makeDOMDriver("#root"),
 };
 function main(sources) {
   const sinks = {
-    DOM: sources.DOM.select('input').events('click')
-      .map(ev => ev.target.checked)
-      .startWith(false)
-      .map(toggled =>
+    DOM: xs.combine(timeNow$, midnight$)
+      .map(([timeNow, {fajir, duhr, asr, maghrib, isha}]) =>
         <div>
-          <input type="checkbox" /> Toggle me
-          <p>{toggled ? 'ON' : 'off'}</p>
+          <h3>{timeNow}</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Prayer</th>
+                <th>Time</th>
+              </tr>
+            </thead>
+            <tbody>
+             <tr>
+               <td>Fajir</td>
+               <td>{fajir}</td>
+             </tr>
+             <tr>
+               <td>Duhr</td>
+               <td>{duhr}</td>
+             </tr>
+             <tr>
+               <td>Asr</td>
+               <td>{asr}</td>
+             </tr>
+             <tr>
+               <td>Maghrib</td>
+               <td>{maghrib}</td>
+             </tr>
+             <tr>
+               <td>Isha</td>
+               <td>{isha}</td>
+             </tr>
+            </tbody>
+          </table>
         </div>
       )
   };
